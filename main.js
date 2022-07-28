9d4eba04314922f20f6555503512b604d1cd1a51
Brian Rono <brishadow24@gmail.com>
2022-07-28 08:40:59 +0300

    my new website

---
+++ main.js
@@ -0,0 +1,29 @@
+let count = 0;
+
+let valueButton = document.querySelector('#value')
+let mySpan = document.querySelector('.myNumber')
+
+
+let buttons = document.querySelectorAll('.btn')
+
+buttons.forEach(function (btn) {
+    btn.addEventListener('click', function (event) {
+        const all = event.currentTarget.classList
+        if (all.contains('increase')) {
+            count++;
+            valueButton.style.color = 'green'
+            valueButton.textContent = count
+        }
+        else if (all.contains('decrease')) {
+            count--;
+            valueButton.style.color = 'red'
+            valueButton.textContent = count
+        }
+        else {
+            count = 0;
+            valueButton.style.color = 'black'
+            valueButton.textContent = count
+        }
+    })
+})
+// js es6 modules
